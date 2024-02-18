f729529a6783a3d7c94e83f64221280e7ce5116f
Selorm Fanah <fanajoy7@gmail.com>
2024-02-17 22:40:25 -0500

    fashion assignment

--- sql/fashion_magazines.sql
+++ sql/fashion_magazines.sql
@@ -1,1 +1,9 @@
 -- Add your sql here
+SELECT customer_name AS Customer_Name, 
+       SUM(subscription_amount) AS Total_Amount_Due
+FROM customers 
+JOIN subscriptions ON customer_id = customer_id
+JOIN magazines ON magazine_id = magazine_id
+WHERE magazine_name = 'Fashion Magazine' 
+  AND subscription_status = 'unpaid'
+GROUP BY customer_name;
